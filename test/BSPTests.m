aadb225ea2628933899aed67ea475050873c1a93
Travis Collins <travis.collins@analog.com>
2018-09-26 21:00:48 -0400

    Update BSPTests.m

--- test/BSPTests.m
+++ test/BSPTests.m
@@ -15,8 +15,10 @@ classdef BSPTests < matlab.unittest.TestCase
         %             addpath(genpath('../hdl_wa_bsp'));
         %         end
         function installBSP(obj)
-            system('wget https://github.com/analogdevicesinc/MathWorks_tools/releases/download/v18.1.0/AnalogDevicesBSP_v18.1.0.mltbx');
-            tbname = 'AnalogDevicesBSP_v18.1.mltbx';
+            %system('wget https://github.com/analogdevicesinc/MathWorks_tools/releases/download/v18.1.0/AnalogDevicesBSP_v18.1.0.mltbx');
+            system('curl -s https://api.github.com/repos/analogdevicesinc/MathWorks_tools/releases/latest | grep browser_download_url | cut -d '"' -f 4 | wget -i -');
+            %tbname = 'AnalogDevicesBSP_v18.1.mltbx';
+            tbname = strrep(regexprep(ls('*0.mltbx'),'[\n\r]+',' '),' ','');
             obj.installed = matlab.addons.toolbox.installToolbox(tbname);
         end
     end
